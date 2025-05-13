2f0445be163164da4b058f2438c996968872904c
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-05-13 14:55:28 -0300

    MongoDB connection + gitignore

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,23 @@
 
 // libs
-const mongoose = requires('mongoose');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
 
 // connection
+const connection = async () =>{
+    try{
+        const dbConnection = await mongoose.connect(
+            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.3ebf5rv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+        )
+        console.log('MongoDB success connection');
+        return dbConnection;
+    }
+    catch(error){
+        console.error('Error at mongoDB connection: ', error);
+    };
+};
+
+connection();
+module.exports = connection
