d8626cbb16d5f62093f6fdb51e72b37be4d5a5a6
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-05-31 10:32:26 -0300

    Register form creation

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -2,11 +2,37 @@
 // css
 import './Auth.css'; 
 
+// hooks
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 
 const Register = () => {
+
+    // handle form
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+    };
+
+
     return (
         <div>
-            <h1>Register</h1>
+            <h2>InstaReact</h2>
+            <p className="subtitle">Cadastre-se para ver as fotos de seus amigos.</p>
+
+            <form onSubmit={ handleSubmit }>
+                <input type="text" placeholder='Nome' />
+                <input type="email" placeholder='Email' />
+                <input type="password" placeholder='Senha' />
+                <input type="password" placeholder='Confirme a senha' />
+            
+                <input type="submit" value="Cadastrar" />
+            </form>
+
+            <p>
+                Já tem conta ?
+                <Link to='/login'>Clique aqui.</Link>
+            </p>
         </div>
     );
 };
