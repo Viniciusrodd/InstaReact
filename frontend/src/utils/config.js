05491dd61abb33334f8c2867697e9ae29f7ec02b
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-06-08 10:50:44 -0300

    Utils Http requisitions config

---
+++ frontend/src/utils/config.js
@@ -0,0 +1,35 @@
+
+export const api = 'http://localhost:5122/api';
+export const upload = 'http://localhost:5122/uploads'
+
+export const requestConfig = (method, data, token = null, image = null) =>{
+    let config;
+
+    if(image){
+        config = {
+            method,
+            body: data,
+            Headers: {}
+        };
+    }else if(method === 'DELETE' || data === null){
+        config = {
+            method,
+            Headers: {}
+        };
+    }else{ // for insertions:
+        config = {
+            method,
+            body: JSON.stringify(data),
+            Headers: {
+                "Content-Type": "application/json"
+            }
+        };
+    }
+
+    // check token
+    if(token){
+        config.Headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+};
