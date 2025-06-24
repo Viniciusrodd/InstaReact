58b5d984f95e98205b3b48f0469c0edd58d011a7
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-06-24 07:39:45 -0300

    Login service

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -29,8 +29,28 @@ const logout = () =>{
 };
 
 
+// login user
+const login = async (data) =>{
+    const config = requestConfig('POST', data);
+    try{
+        const res = await fetch(api + '/users/login', config)
+            .then((res) => res.json())
+            .catch((error) => error);
+
+        if(res){
+            localStorage.setItem('user', JSON.stringify(res));
+        }
+
+        return res;
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+
 const authService = {
-    register, logout
+    register, logout, login
 };
 
 
