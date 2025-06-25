1ccc189caaf4ef1c390ccc1ccc69a5b6e9b67cc5
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-06-25 11:00:30 -0300

    Update profile slice

--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -26,6 +26,19 @@ export const profile = createAsyncThunk('user/profile', async (user, thunkAPI) =
 });
 
 
+// update user details
+export const updateProfile = createAsyncThunk('user/update', async (user, thunkAPI) =>{
+    const token = thunkAPI.getState().auth.user?.token;
+
+    const data = await userService.updateProfile(user, token);
+    if(data.errors){
+        return thunkAPI.rejectWithValue(data.errors); // rejecting the request
+    }
+    
+    return data;
+});
+
+
 // slice
 export const userSlice = createSlice({
     name: 'user',
@@ -46,7 +59,23 @@ export const userSlice = createSlice({
             state.success = true;
             state.error = null;
             state.user = action.payload;
-        });
+        })
+        .addCase(updateProfile.pending, (state) =>{
+            state.loading = true,
+            state.error = false;
+        })
+        .addCase(updateProfile.fulfilled, (state, action) =>{
+            state.loading = false,
+            state.success = true;
+            state.error = null;
+            state.user = action.payload;
+            state.message = 'Usuário atualizado com sucesso!'
+        })
+        .addCase(updateProfile.rejected, (state, action) =>{
+            state.loading = false,
+            state.error = action.payload;
+            state.user = null
+        })
     }
 });
 
