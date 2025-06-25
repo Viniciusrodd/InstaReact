4139dd3d397dd93dbf973fac548adde666a8d406
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-06-25 10:43:58 -0300

    Profile image exibition

--- frontend/src/pages/EditProfile/EditProfile.jsx
+++ frontend/src/pages/EditProfile/EditProfile.jsx
@@ -56,6 +56,16 @@ const EditProfile = () => {
 
     };
 
+    // set image values
+    const handleFile = (e) =>{
+        // image preview
+        const image = e.target.files[0];
+        setPreviewImage(image);
+
+        // update image state
+        setProfileImage(image);
+    };
+
 
     /////// jsx
 
@@ -66,14 +76,20 @@ const EditProfile = () => {
             <p className="subtitle">
                 Adicione uma imagem de perfil, e conte mais sobre você...
             </p>
-            {/* image preview */}
+            { (user.profileImage || previewImage) && (
+                <img 
+                    className='profile-image'
+                    src={ previewImage ? URL.createObjectURL(previewImage) : `${upload}/users/${user.profileImage}` }
+                    alt={ user.name }
+                />
+            ) }
         
             <form onSubmit={ handleSubmit }>
                 <input type="text" placeholder='Nome' value={ name || '' } onChange={(e) => setName(e.target.value)} />
                 <input type="email" placeholder='Email' disabled value={ email || '' } />
                 <label>
                     <span>Imagem do perfil: </span>
-                    <input type="file" />
+                    <input type="file" onChange={ handleFile } />
                 </label>
                 <label>
                     <span>Bio: </span>
