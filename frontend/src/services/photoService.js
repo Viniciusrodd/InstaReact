b65e5eb643e9368b11740ed8bb08af555ef4d4df
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-06-27 10:12:31 -0300

    Publish user photo service

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -3,8 +3,25 @@
 import { api, requestConfig } from "../utils/config";
 
 
+// publish an user photo
+const publishPhoto = async (data, token) =>{
+    const config = requestConfig('GET', data, token, true);
+
+    try{
+        const res = await fetch(api + '/photos', config)
+            .then((res) => res.json())
+            .catch((error) => error)
+
+        return res;
+    }
+    catch(error){
+        console.log(error);
+    }
+};
+
+
 // export service
 const photoService = {
-    
+    publishPhoto
 };
 export default photoService;
