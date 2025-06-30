87f63164033abaefdfc19f391e2162348335fac0
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-06-30 11:00:53 -0300

    Edit photo slice

--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -54,6 +54,19 @@ export const deletePhoto = createAsyncThunk('photo/delete', async (id, thunkAPI)
 });
 
 
+// update a photo
+export const updatePhoto = createAsyncThunk('/photo/update', async (photoData, thunkAPI) =>{
+    const token = thunkAPI.getState().auth.user?.token;
+
+    const data = photoService.updatePhoto({ title: photoData.title }, photoData.id, token);
+    if(data.errors){
+        return thunkAPI.rejectWithValue(data.errors);
+    }
+
+    return data;
+});
+
+
 // slice
 export const photoSlice = createSlice({
     name: 'photo',
@@ -100,12 +113,37 @@ export const photoSlice = createSlice({
             state.loading = false;
             state.success = true;
             state.error = null;
+            
             state.photos = state.photos.filter((photo) =>{
                 return photo._id !== action.payload.id
             });
-            state.message = action.payload.message
+            
+            state.message = action.payload.message;
         })
         .addCase(deletePhoto.rejected, (state) =>{
+            state.loading = false;
+            state.error = action.payload;
+            state.photo = null;
+        })
+        .addCase(updatePhoto.pending, (state) =>{
+            state.loading = true;
+            state.error = false;
+        })
+        .addCase(updatePhoto.fulfilled, (state, action) =>{
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+
+            state.photos.map((photo) =>{
+                if(photo._id === action.payload.photo._id){
+                    return photo.title === action.payload.photo.title
+                }
+                return photo;
+            });
+
+            state.message = action.payload.message;
+        })
+        .addCase(updatePhoto.rejected, (state) =>{
             state.loading = false;
             state.error = action.payload;
             state.photo = null;
