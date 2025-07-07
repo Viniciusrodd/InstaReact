be889db047b03ed77285dfea041e16f486da3a16
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-07-07 14:47:59 -0300

    Homepage structuring + style

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -18,7 +18,7 @@ import { getPhotos, like } from '../../slices/photoSlice';
 const Home = () => {
     // consts
     const dispatch = useDispatch();
-    const resetMessage = useResetComponentMessage(); // custom hook
+    const resetMessage = useResetComponentMessage(dispatch); // custom hook
     const { user } = useSelector((state) => state.auth);
     const { photos, loading } = useSelector((state) => state.photo);
 
@@ -48,8 +48,23 @@ const Home = () => {
 
 
     return (
-        <div>
-            <h1>Homepage</h1>
+        <div id='home'>
+            { photos && photos.map((photo) => (
+                <div key={ photo._id }>
+                    <PhotoItem photo={ photo } />
+                    <LikeContainer photo={ photo } user={ user } handleLike={ handleLike } />
+                    <Link className='btn' to={`/photos/${photo._id}`}>
+                        Ver mais
+                    </Link>
+                </div>
+            )) }
+
+            { photos && photos.length === 0 && (
+                <h2 className='no-photos'>
+                    Ainda não há fotos publicadas, 
+                    <Link to={`/users/${user._id}`}>clique aqui</Link>
+                </h2>
+            ) }
         </div>
     );
 };
